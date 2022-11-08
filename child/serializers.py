0e43f207a68abaee87fee2697fec3c5febff7c87
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-11-08 16:25:38 -0300

    removendo pai do relacionamento

--- child/serializers.py
+++ child/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from .utils import generate_code
 from core.models import Child
-from user.serializers import UserSerializer
 from core.serializers import ClassesSerializer
 
 class ChildSerializer(serializers.ModelSerializer):
@@ -22,10 +21,9 @@ class ChildSerializer(serializers.ModelSerializer):
 
 
 class ReadChildSerializer(serializers.ModelSerializer):
-    father = UserSerializer(read_only=True)
     class_id = ClassesSerializer(read_only=True)
 
     class Meta:
         model = Child
-        fields = ['id', 'name', 'code', 'class_id', 'father']
+        fields = ['id', 'name', 'code', 'class_id']
         read_only_fields = fields
