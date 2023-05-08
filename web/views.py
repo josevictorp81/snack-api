09fcbc028fc96fc850e5602d240e330c9534cbc2
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-05-08 10:17:49 -0300

    listando classes e lanches

--- web/views.py
+++ web/views.py
@@ -1,13 +1,28 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+# from braces.views import SuperuserRequiredMixin
 
-class OrderListView(TemplateView):
+from core.models import Order, Snack, Classes
+
+class OrderListView(ListView):
+    model = Order
     template_name = 'order_list.html'
+
 
 class StudentListView(TemplateView):
     template_name = 'student_list.html'
 
-class SnackListView(TemplateView):
-    template_name = 'snack_list.html'
 
-class ClassListView(TemplateView):
+class SnackListView(ListView):
+    context_object_name = 'snack_list'
+    template_name = 'snack_list.html'
+    model = Snack
+
+
+class ClassListView(ListView):
+    context_object_name = 'class_list'
+    model = Classes
     template_name = 'class_list.html'
