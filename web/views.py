01b028aa03d7218e7f9e411a232ad95ab79939d8
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-05-10 10:10:28 -0300

    adicionando paginção

--- web/views.py
+++ web/views.py
@@ -13,15 +13,18 @@ class StudentListView(ListView):
     template_name = 'student_list.html'
     context_object_name = 'student_list'
     model = Child
+    paginate_by = 25
 
 
 class SnackListView(ListView):
     context_object_name = 'snack_list'
     template_name = 'snack_list.html'
     model = Snack
+    paginate_by = 25
 
 
 class ClassListView(ListView):
     context_object_name = 'class_list'
     model = Classes
     template_name = 'class_list.html'
+    paginate_by = 25
