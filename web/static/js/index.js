9accd8b529b77f8c6cc91fb4be50e71035c6852c
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-05-28 12:07:39 -0300

    mensagem saindo automaticamente

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,15 +1,10 @@
 // close messages
-
 function closeMessagesAuto() {
-    console.log('saiu')
     setTimeout(() => {
         document.querySelector('.messages').style.display = 'none'
-        console.log('saiu 2')
+        // document.querySelector('.messages').style.transition = '2s'
+        // document.querySelector('.messages').style.transitionProperty = 'display'
     }, 3000)
-}
-
-function closeMessages() {
-    document.querySelector('.messages').style.display = 'none'
 }
 
 // close delete modal
